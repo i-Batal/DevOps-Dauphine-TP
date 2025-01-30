f149a921483dda99a1307b09a68563927ec5a897
i-Batal <ilyess.batal7@gmail.com>
2025-01-30 10:20:22 +0000

    Fin Partie - debut partie bonus

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,9 @@ terraform {
   
   backend "gcs" {
     bucket  = "dauphine-tpnote-ib"  # Le nom du bucket
-    prefix  = "terraform/state"       # Chemin "logique" dans le bucket
+    prefix  = "terraform/state"     # Chemin "logique" dans le bucket
   }
 
-  # Si besoin, vous pouvez spécifier un provider requérant une version minimale
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -22,15 +21,11 @@ resource "google_project_service" "enable_apis" {
     "artifactregistry.googleapis.com",
     "sqladmin.googleapis.com",
     "cloudbuild.googleapis.com",
-
-    "run.googleapis.com" #ajouté pour la partie 3
-
+    "run.googleapis.com"
   ])
 
   project = var.project_id
   service = each.key
-  
-  # On ne désactive pas le service à la destruction de la ressource
   disable_on_destroy = false
 }
 
@@ -62,7 +57,7 @@ resource "google_cloud_run_service" "wordpress" {
   template {
     spec {
       containers {
-        image = "us-central1-docker.pkg.dev/tp5-devops-dauphine/website-tools/wordpress-custom" # Remplacez par votre image Docker
+        image = "us-central1-docker.pkg.dev/tp5-devops-dauphine/website-tools/wordpress-custom"
         ports {
           container_port = 80
         }
@@ -80,7 +75,7 @@ resource "google_cloud_run_service" "wordpress" {
         }
         env {
           name  = "WORDPRESS_DB_HOST"
-          value = "104.154.20.193" # Adresse IP publique MySQL
+          value = "104.154.20.193"  # Adresse IP publique MySQL
         }
       }
     }
@@ -102,4 +97,151 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   service     = google_cloud_run_service.wordpress.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
+
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = "gke-dauphine"
+  location = "us-central1-a"
+}
+
+provider "kubernetes" {
+  host                   = data.google_container_cluster.my_cluster.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth.0.cluster_ca_certificate)
+}
+
+# Déploiement MySQL
+resource "kubernetes_deployment" "mysql" {
+  metadata {
+    name = "mysql"
+    labels = {
+      app = "mysql"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "mysql"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "mysql"
+        }
+      }
+      spec {
+        container {
+          name  = "mysql"
+          image = "mysql:5.7"
+          env {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = "rootpassword"
+          }
+          env {
+            name  = "MYSQL_DATABASE"
+            value = "wordpress"
+          }
+          env {
+            name  = "MYSQL_USER"
+            value = "wordpress"
+          }
+          env {
+            name  = "MYSQL_PASSWORD"
+            value = "ilovedevops"
+          }
+          port {
+            container_port = 3306
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "mysql" {
+  metadata {
+    name = "mysql-service"
+  }
+  spec {
+    selector = {
+      app = "mysql"
+    }
+    port {
+      port        = 3306
+      target_port = 3306
+    }
+    type = "LoadBalancer"
+  }
+}
+
+
+# Déploiement WordPress
+resource "kubernetes_deployment" "wordpress" {
+  metadata {
+    name = "wordpress"
+    labels = {
+      app = "wordpress"
+    }
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "wordpress"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "wordpress"
+        }
+      }
+      spec {
+        container {
+          name  = "wordpress"
+          image = "us-central1-docker.pkg.dev/tp5-devops-dauphine/website-tools/wordpress-custom"
+          env {
+            name  = "WORDPRESS_DB_HOST"
+            value = "mysql-service"
+          }
+          env {
+            name  = "WORDPRESS_DB_USER"
+            value = "wordpress"
+          }
+          env {
+            name  = "WORDPRESS_DB_PASSWORD"
+            value = "ilovedevops"
+          }
+          env {
+            name  = "WORDPRESS_DB_NAME"
+            value = "wordpress"
+          }
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "wordpress" {
+  metadata {
+    name = "wordpress-service"
+  }
+  spec {
+    selector = {
+      app = "wordpress"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
+}
+
 
